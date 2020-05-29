a060c1f34dd0900419e53c6d300c47975529c66e
sbast <sbast@192.168.1.136>
2020-05-29 16:16:02 -0500

    Primer Commit: Alteracion de application.properties

---
+++ LlamaPoliceAppWeb/src/main/java/pe/gob/LlamaPoliceAppWebApplication.java
@@ -0,0 +1,13 @@
+package pe.gob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LlamaPoliceAppWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LlamaPoliceAppWebApplication.class, args);
+	}
+
+}
