f8b27a03c629a4db6315e1420b6db6a4a2bd3433
sbast <sbast@192.168.1.136>
2020-06-04 09:00:18 -0500

    Comisaria Controller

--- LlamaPoliceAppWeb/src/main/java/pe/gob/repository/ComisariaRepository.java
+++ LlamaPoliceAppWeb/src/main/java/pe/gob/repository/ComisariaRepository.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import pe.gob.model.Comisaria;
 
+@Repository
 public interface ComisariaRepository extends JpaRepository<Comisaria, Integer>{
 	
 	@Query("from Comisaria c where c.nombreComisaria like %:nombreComisaria%")
