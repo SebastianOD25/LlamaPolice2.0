61af72fee2de22b200d0dc985193f8a8dd057c5c
sbast <sbast@192.168.1.136>
2020-06-04 07:58:20 -0500

    Comisaria Service completo

--- LlamaPoliceAppWeb/src/main/java/pe/gob/service/ComisariaService.java
+++ LlamaPoliceAppWeb/src/main/java/pe/gob/service/ComisariaService.java
@@ -1,5 +1,22 @@
 package pe.gob.service;
 
-public interface ComisariaService {
+import java.util.List;
+import java.util.Optional;
 
+import pe.gob.model.Comisaria;
+
+public interface ComisariaService {
+	
+	public boolean insertar(Comisaria comisaria);
+	
+	public boolean modificar(Comisaria comisaria);
+	
+	public void eliminar(int idComisaria);
+	
+	public Optional<Comisaria> listarId(int idComisaria);
+	
+	List<Comisaria> lista();
+	
+	List<Comisaria> buscarNombre(String nomComisaria);
+	
 }
---
+++ LlamaPoliceAppWeb/src/main/java/pe/gob/serviceimpl/ComisariaServiceImpl.java
@@ -0,0 +1,67 @@
+package pe.gob.serviceimpl;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import pe.gob.model.Comisaria;
+import pe.gob.repository.ComisariaRepository;
+import pe.gob.service.ComisariaService;
+
+@Service
+public class ComisariaServiceImpl implements ComisariaService{
+	
+	@Autowired
+	private ComisariaRepository rComisaria;
+
+	@Override
+	@Transactional
+	public boolean insertar(Comisaria comisaria) {
+		Comisaria objComisaria = rComisaria.save(comisaria);
+		if(objComisaria == null) {
+			return false;
+		}else
+		return true;
+	}
+
+	@Override
+	@Transactional
+	public boolean modificar(Comisaria comisaria) {
+		boolean flag = false;
+		try {
+			rComisaria.save(comisaria);
+			flag = true;
+		} catch (Exception ex) {
+			System.out.println("Ocurrio un error, intentelo de nuevo");
+		}
+		return flag;
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public void eliminar(int idComisaria) {
+		rComisaria.deleteById(idComisaria);
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public Optional<Comisaria> listarId(int idComisaria) {
+		return rComisaria.findById(idComisaria);
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public List<Comisaria> lista() {
+		return rComisaria.findAll();
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public List<Comisaria> buscarNombre(String nomComisaria) {
+		return rComisaria.buscarNombre(nomComisaria);
+	}
+
+}
