8e68c4e53942726c86da75d45314c6e5748d7703
sbast <sbast@192.168.1.136>
2020-06-09 20:25:28 -0500

    Persona Repository

---
+++ LlamaPoliceAppWeb/src/main/java/pe/gob/service/PersonaService.java
@@ -0,0 +1,18 @@
+package pe.gob.service;
+
+import java.util.List;
+import java.util.Optional;
+import pe.gob.model.Persona;
+
+
+public interface PersonaService {
+	
+	public boolean insertar(Persona persona);
+	public boolean modificar(Persona persona);
+	public void eliminar(int idPersona);
+	public Optional<Persona> buscarId(int idPersona);
+	public Optional<Persona> listarId(int idPersona);
+	public List<Persona> listar();
+	public List<Persona> buscarNombre(String namePersona);
+	
+}
